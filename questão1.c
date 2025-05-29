0fd3246647c8c3b7f4dda90bc6cef9cea3b75db2
Ana luisa ferreira <164504412+vngss@users.noreply.github.com>
2025-05-29 11:37:29 -0300

    Update questão1.c

--- questão1.c
+++ questão1.c
@@ -5,17 +5,14 @@
 int main() {
     float numero1, numero2, soma;
 
-    // Solicita os números ao usuário
     printf("Digite o primeiro número: ");
     scanf("%f", &numero1);
 
     printf("Digite o segundo número: ");
     scanf("%f", &numero2);
-
-    // Realiza a soma
     soma = numero1 + numero2;
 
-    // Exibe o resultado
+ 
     printf("A soma de %.2f + %.2f é = %.2f\n", numero1, numero2, soma);
 
     return 0;
