4f75a9792cd2ce565bdd6c585f259604ba5d7f98
Ana luisa ferreira <164504412+vngss@users.noreply.github.com>
2025-05-29 11:06:17 -0300

    Create questã027.c

---
+++ questã027.c
@@ -0,0 +1,21 @@
+Fazer um programa que lê o preço de um produto e inflaciona esse preço em
+10% se ele for menor que 100 e em 20% se ele for maior ou igual a 100.
+
+#include <stdio.h>
+
+int main() {
+    float preco, novoPreco;
+
+    printf("Digite o preço do produto: R$ ");
+    scanf("%f", &preco);
+
+    if (preco < 100) {
+        novoPreco = preco * 1.10; // Aumenta 10%
+    } else {
+        novoPreco = preco * 1.20; // Aumenta 20%
+    }
+
+    printf("O novo preço do produto é: R$ %.2f\n", novoPreco);
+
+    return 0;
+}
