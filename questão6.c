269ef554df024529be66e805a8abdfdaa50c4e86
Ana luisa ferreira <164504412+vngss@users.noreply.github.com>
2025-05-29 10:55:26 -0300

    Create questão6.c

---
+++ questão6.c
@@ -0,0 +1,24 @@
+ Escreva um programa que determine se um número digitado pelo usuário é uma potência de 2.
+
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    if (numero <= 0) {
+        printf("O número deve ser positivo.\n");
+        return 0;
+    }
+
+    // Verifica se o número é uma potência de 2
+    if ((numero & (numero - 1)) == 0) {
+        printf("%d é uma potência de 2.\n", numero);
+    } else {
+        printf("%d NÃO é uma potência de 2.\n", numero);
+    }
+
+    return 0;
+}
